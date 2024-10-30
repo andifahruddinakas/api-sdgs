675851945a99c906c40adb9013a9dc0da5a9aa22
Andi Fahruddin Akas <57283157+andifahruddinakas@users.noreply.github.com>
2024-10-30 15:32:26 +0800

    minify

--- generator/index.js
+++ generator/index.js
@@ -2,7 +2,6 @@ const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 
-// Fungsi untuk memeriksa cache desa
 const checkCache = (desaCode) => {
   const cachePath = path.join(__dirname, "./data/cache_sdgs.json");
   if (fs.existsSync(cachePath)) {
@@ -12,44 +11,39 @@ const checkCache = (desaCode) => {
   return false;
 };
 
-// Fungsi untuk memperbarui cache desa
 const updateCache = (desaCode) => {
   const cachePath = path.join(__dirname, "./data/cache_sdgs.json");
   let cacheData = {};
 
-  // Membaca data cache yang sudah ada
   if (fs.existsSync(cachePath)) {
     cacheData = JSON.parse(fs.readFileSync(cachePath, "utf-8"));
   }
 
-  // Inisialisasi array desa jika belum ada
   if (!cacheData.desa) {
     cacheData.desa = [];
   }
 
-  // Tambahkan kode desa ke cache jika belum ada
   if (!cacheData.desa.includes(desaCode)) {
     cacheData.desa.push(desaCode);
   }
 
-  fs.writeFileSync(cachePath, JSON.stringify(cacheData, null, 2));
+  fs.writeFileSync(cachePath, JSON.stringify(cacheData));
 };
 
-// Fungsi untuk menyimpan data desa sebagai JSON
 const saveJsonData = (desaCode, data) => {
-  const outputDir = path.join(__dirname, `./public/desa`);
+  const outputDir = path.join(__dirname, "../public/desa");
   if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir, { recursive: true });
   }
 
   const outputFilePath = path.join(outputDir, `${desaCode}.json`);
-  fs.writeFileSync(outputFilePath, JSON.stringify(data, null, 2));
+
+  fs.writeFileSync(outputFilePath, JSON.stringify(data));
   console.log(
-    `Data untuk desa ${desaCode} berhasil disimpan di ${outputFilePath}`
+    `Data untuk desa ${desaCode} berhasil disimpan.`
   );
 };
 
-// Fungsi untuk mengambil dan menyimpan data dari CSV
 const fetchAndSaveDataFromCsv = async (csvFilePath) => {
   const csvData = fs.readFileSync(csvFilePath, "utf-8").split("\n").slice(1);
 
@@ -57,9 +51,7 @@ const fetchAndSaveDataFromCsv = async (csvFilePath) => {
     const [locationCode] = row.split(",");
 
     if (checkCache(locationCode)) {
-      console.log(
-        `Data untuk desa ${locationCode} sudah ada di cache.`
-      );
+      console.log(`Data untuk desa ${locationCode} sudah ada di cache.`);
       continue;
     }
 
@@ -68,20 +60,18 @@ const fetchAndSaveDataFromCsv = async (csvFilePath) => {
         `https://sid.kemendesa.go.id/sdgs/searching/score-sdgs?location_code=${locationCode}`
       );
 
-      // Cek status respons
       if (response.status !== 200) {
-        console.log(`Status ` + response.status + ` untuk desa ${locationCode}.`);
+        console.log(`Status ${response.status} untuk desa ${locationCode}.`);
         continue;
       }
 
       let scoreData = response.data;
 
-      // Modifikasi data sesuai format yang diinginkan
       if (scoreData.data) {
         scoreData.data = scoreData.data.map((item) => ({
           goals: item.goals,
           title: item.title,
-          image: item.image.split("/").pop(), // Mengambil nama file dari URL
+          image: item.image.split("/").pop(),
           score: item.score,
         }));
       }
@@ -98,7 +88,6 @@ const fetchAndSaveDataFromCsv = async (csvFilePath) => {
   }
 };
 
-// Fungsi untuk mengambil dan menyimpan data dari semua CSV di folder data
 const fetchAndSaveAllData = async () => {
   const dataFolder = path.join(__dirname, "./data/");
   const files = fs.readdirSync(dataFolder);
@@ -115,10 +104,8 @@ const fetchAndSaveAllData = async () => {
   }
 };
 
-// Fungsi utama
 const main = async () => {
   await fetchAndSaveAllData();
 };
 
-// Menjalankan fungsi utama
 main();
